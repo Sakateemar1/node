dfa13dc22dea794dbc1dc36a1c01694bdd38e487
Sakateemar1 <simplyteemar@gmail.com>
2023-07-31 13:32:13 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,494 @@
+// // // // // //console.log("Hello World")
+// // // // // //const firstName = "Fatima"
+// // // // // //console.log(firstName)
+
+// // // // // //an array
+// // // // // const students = [
+// // // // // "Tope",
+// // // // // "Seyi",
+// // // // // "Tola",
+// // // // // "Tayo",
+// // // // // "Dupe",
+// // // // // ]
+// // // // // //to add an item to the end of the array
+// // // // // students.push("Dayo, Titi")
+
+// // // // // // to remove an item from  the array
+// // // // // students.pop("Titi")
+
+
+
+
+// // // // // //console.log(students)
+// // // // // const String = students.toString
+// // // // // console.log(students)
+
+
+
+// // // // // //numbers
+// // // // // let quantity =40
+// // // // // let price=200
+// // // // // console.log(quantity * price)
+
+// // // // // //String
+// // // // // let firstName ="Fatima"
+// // // // // let lastName = "Saka"
+// // // // // let statement = "is a student"
+// // // // // console.log(firstName + lastName + statement)
+
+// // // // // // Boolean
+// // // // // //True/false
+
+// // // // // //fun(number,ction
+// // // // // const p = 40
+// // // // // const y = 45
+
+// // // // // function myfunction(p,y){
+// // // // // var p = 6
+// // // // // var y = 6
+// // // // // return p * y
+// // // // // }
+    
+// // // // // console.log(myfunction[p,y])
+
+// // // // // //Object
+
+// // // // // const person= {
+// // // // //     firstName:"David",
+// // // // //     lastName: "Williams",
+// // // // //     age: "10",
+// // // // //     height:"15",
+// // // // //     weight:"4000",
+
+// // // // // }
+// // // // // console.log(person.weight)
+
+// // // // // //Array
+// // // // // const student=[
+// // // // //     "Tunji",
+// // // // //     "Tola",
+// // // // //     "Temi",
+// // // // //     "Adenike",
+// // // // //     "Tayo",
+
+// // // // // ]
+// // // // // console.log(student[3])
+
+// // // // // //multiple objects in an array
+
+// // // // // const cars=[
+// // // // // {
+// // // // //     name: "benz",
+// // // // //     capacity: "6",
+// // // // //     color:"black",
+// // // // //     model:"1",
+
+// // // // // },
+// // // // // {
+// // // // //     name: "volvo",
+// // // // //     capacity: "4",
+// // // // //     color:"yellow",
+// // // // //     model:"2",
+// // // // // },
+// // // // // {
+// // // // //     name: "honda",
+// // // // //     capacity: "6",
+// // // // //     color:"red",
+// // // // //     model:"3",
+// // // // // },
+// // // // // {
+// // // // //     name: "toyota",
+// // // // //     capacity: "6",
+// // // // //     color:"blue,",
+// // // // //     model:"4",
+
+// // // // // }
+// // // // // ]
+// // // // // let car ={
+// // // // //     name: 'volks',
+// // // // //     capacity: '4',
+// // // // //     color: 'red',
+// // // // //     model: "3",
+// // // // // }
+// // // // // console.log(cars.unshift(cars))
+// // // // // console.log(cars)
+// // // // // let sizes = cars.map(car=>{
+// // // // //     if(car.capacity <=3){
+// // // // //         return "small";
+// // // // //     }
+// // // // //     if (car.capacity <=5){
+// // // // //         return "medium";    
+// // // // //     }
+// // // // //     return "large";
+// // // // // })
+// // // // // console.log(sizes);
+
+// // // // // //Conditional Statement
+// // // // // //if statement
+// // // // // //const hour = "2"
+// // // // // //if(hour <18){
+// // // // //   //  var greeting = "Good Morning"
+// // // // // //}
+// // // // // //console.log(greeting)
+
+// // // // // //if else statement
+// // // // // //const hour = "2"
+// // // // // //if(hour < 10 ){
+// // // // //    // var greetings = "Good Morning"
+// // // // // //}else{
+// // // // //     //var greetings ="Good evening"
+// // // // // //}
+// // // // // //console.log(greetings)
+
+// // // // // //else if Statement
+
+// // // // // // const hour = "2"
+// // // // // // if(hour < 10 ){
+// // // // // //     var greetings = "Good Morning"
+// // // // // // }else if(hour <20){
+// // // // // //     var greetings = "Good Day"
+
+// // // // // // }else{
+// // // // // //     var greetings ="Good evening"
+// // // // // // }
+// // // // // //  console.log(greetings)
+// // // // //  //exercise example of if
+// // // // // //  const height ="7"
+// // // // // //  if (height  <=7 ){
+// // // // // //     var Tola ="Tola is Tall"
+// // // // // //  }
+// // // // // // console.log (Tola)
+
+
+// // // // // // // // example of Else if
+// // // // // // const height ="6"
+// // // // // //   if (height <6 ){
+// // // // // //      var Tola =  "Tola is short"
+// // // // // //   }else if (height >=7){
+// // // // // //      var Tola = "Tola is tall"
+// // // // // //   }else {
+// // // // // //     var Tola = "Tola has an Average Height"
+// // // // // //   }
+// // // // // //  console.log(Tola)
+
+// // // // // // //example of else
+// // // // //  const height ="7"
+// // // // //  if (height > 7 ){
+// // // // //  var Tola =  "Tola is short"
+// // // // //  }else if (height < 7){
+// // // // //     var Tola = "Tola is tall"
+// // // //  //}else {
+// // // //   // var Tola = "Tola has an Average Height"
+// // // //  //}
+// // // //  //console.log (Tola)
+
+// // // // // function myfucntion(){
+// // // // //     if (age =10) {
+// // // // //       var studen  
+// // // // //     }
+// // // // //     var studentAge = 
+// // // // //     if
+// // // // // }
+// // // // // function myfunction(){
+// // // // //     console.log("today is good");
+// // // // //     try{    
+// // // // //         throw new Error ("This is error");
+// // // // //     }catch (error){
+// // // // //         console.log(error);
+// // // // //     }
+// // // // // }
+
+
+// // // //  //if (height > 7 ){
+// // // //  //var Tola =  "Tola is short"
+// // // //  //}else if (height < 7){
+   
+// // // // // var Tola = "Tola is tall"
+
+// // // //     //simple server
+// // // // //     const http1 =require('node:http');
+// // // // //    const port =8080;
+// // // // //    const hostname = '127.0.0.1' //local host
+// // // // //    const server = http1.createServer(
+// // // // //     (request, response) => {
+// // // // //         response.setHeader('content-type', 'text/plain');
+// // // // //         response.statusCode = 200
+// // // // //         response.end('hello world');
+// // // // //     }
+
+
+// // // // //    )
+// // // // // server.listen(port,hostname, ( ) => {
+// // // // //    console.log('server is running') 
+
+// // // // // })
+
+// // // // //Exercise (Conditional Statement)
+// // // // const hour=24
+// // // //     if (hour >24 ){
+// // // //         var day = " More than a day"
+// // // //     }else if (hour <24){
+// // // //         var day ="It not up to a day"
+// // // //     }else { var day ="It is a day"
+
+// // // //     }
+// // // // console.log(day)
+
+// // // // //Array declaration
+
+// // // // const employee =[ "Adebayo", 
+// // // // "Adegoke",
+// // // //  "Adenike",
+// // // //  "Oluwaseyi", 
+// // // //  "Olagoke",
+// // // //  "Tomisin",
+// // // //  "Yetunde",
+// // // //  "Fatima",
+// // // //  "Micehal",
+// // // // ]
+// // // // console.log(employee)
+
+// // // // //ObjectArray
+// // // // const customers= [{
+// // // //     name: "Adetutu",
+// // // //     age: 20,
+// // // //     address: "Lekki"  
+// // // // },
+// // // // {   name: "Olasile",
+// // // //     age: 26,
+// // // //     address: "Ajah"
+    
+// // // // },
+// // // // {   name: "Adeola",
+// // // //     age: 29,
+// // // //     address: "Ologolo"
+// // // // },
+// // // // {   name: "Olawale",
+// // // //     age: 30,
+// // // //     address: "Victoria Island"
+// // // // }
+// // // // ]
+// // // // console.log(customers)
+
+// // // // // function
+// // // // const studentName= (" Olawale");
+// // // // const studentAge=  25;
+// // // // const examScore= 18;
+// // // // const testScore=30
+// // // // const totalScore=(examScore + testScore);
+
+// // // // console.log(totalScore)
+// // // // console.log(studentAge)
+
+// // // // function getScore(){
+
+// // // //     if  (examScore >20){
+// // // //         return studentName +  "Pass";
+// // // //     }else if (examScore <=20) {
+// // // //         return studentName  + "fail";
+// // // //     }   
+    
+// // // // } 
+// // // // console.log(getScore())
+
+
+
+// // //  //example asynchronous function timer
+// // //  function fetchDataFromServer(){
+
+// // //     //simulating asynchronous programm
+// // //     setTimeout(() =>{
+// // //         const data = { message: "Hello World"
+// // //     }
+  
+    
+// // //         const error = null;
+
+// // //         if(error){
+            
+// // //             callback (error);
+
+// // //         }else {console.log}
+
+
+// //      //exercise a  callback function
+
+// //      //function  getUser(callback){
+
+// //     //    callback("Fatima")
+    
+
+     
+// // //     // simple callbck function
+// // //     function greet(name, callback){
+// // //  console.log( " Hi" + ' ' +name);
+// // //      callback();
+    
+// // //      }
+// // //      //callback function
+// // //      function callMe(){
+// // //          console.log("I am callbck function");
+// // //      }
+// // //         //passing function as an arguement
+// // //          greet("Fatima", callMe);
+
+// // //example of  a block scope
+// // // let age=20
+// // // let year=3
+// // // function ageCalculation( number){
+    
+          
+// // // console.log(ageCalculation())
+
+// // //simple function
+// // //function stateGender(name,callback){
+// //     //console.log("name +  ' ' + Female");
+// //     //callback();
+
+// // //callback function
+// //  //function myGender(){
+// //  //console.log("is a girl");
+ 
+ 
+// //  //passing function as an arguement
+// //  //stateGender("Fatima, stateGender")
+
+// //  //simple callback function
+// // //  let age=12
+// // //  function myName(showMyName){ 
+// // //     showMyName()
+// // //  }
+
+// // //  function theShowMyName(){ 
+// // //     let name = "Fatima"
+// // //   console.log(name  + ' ' + 'is' + ' ' + age+' ' +"years Old" )
+// // //  }
+// // //     myName(theShowMyName)
+// // //  const arr=[1,2,3,4,5];
+// // //  arr.forEach(num=>console.log(num));
+
+// // // const person={
+// // //    name:"john",
+// // //    age: 30,
+// // //    address: "lekki"
+// // // };
+// // // console.log(person.address)
+
+// // // const multiply=(a,b)=>{
+// // //    return a*b;
+// // // };
+// // // console.log(multiply(2,3,4));
+    
+// // // console.log(typeOfNan)
+
+// // console.log("This is a list of an array");
+// // //an array
+// // let studentNames = ["Ajibola", 'Ololade', 'Akinlade', 'Anuoluwapo', 'Omotunde']; 
+// // // to add an element to end of the array
+// // studentNames.push('Adebayo');
+// // //add an element to the  beginning of the array
+// //  studentNames.unshift('Olawale');
+// //  //add an elemenmt to a particular spot in the array
+// //  studentNames.splice(2,0,'Adeola'),
+// //  console.log(studentNames);
+// //  console.log(studentNames[4]+ ' '+"is the oldest on the list");
+
+// //  //an object
+// //  const customerDetails =[{Name: "bode",
+// // Age: 22,
+// // phone: 9019276647,
+// // address: "lekki"},
+
+// // {Name: "Wale",
+// // Age: 35,
+// // phone: 9093286647,
+// // address: "Mowe"
+
+// // },
+// // {Name: "Yinka",
+// // Age: 22,
+// // phone: 90183902387,
+// // address: "Idiroko"
+
+// // }
+// //  ]
+// // console.log({customerDetails});
+// // //to access the property of the object
+// // customerDetails.forEach(customerDetails=>{
+// //     if (customerDetails.Age===22){
+// //         console.log("is young");
+// //     }else if (customerDetails.Age===25){
+// //         console.log("is old")
+// //     }else console.log("is Older")
+// // })
+    
+// //  //nesting an array in an object
+// //  const senators=[{name:"Anikulapo",
+// //      age:50,
+// //      cars:['Lexus:RX 350', 'RX 450', "RX 230"]
+// //      } ]
+
+// //  console.log (senators);
+
+// //  //nestin an object in an array
+// //  const nailDeptStaff= ["HOD", "Pedicurist", "NailTechnician",
+// //  nailDeptStaff.forEach(nailDeptStaff=>{
+// //     if (nailDeptStaff==="HOD"){
+// //     console.log("Tayo")
+// //     }else if (nailDeptStaff==="Pedicurist"){
+// //         console.log("Fatima");    
+// //     }else console.log ("Adenike")
+// //  }),
+// // ]
+
+// //simple function
+// function welcome(name, acollade){
+//     console.log("Hello"+' '+name);
+//     acollade()
+// }
+// //callback function
+//  function acollade(){
+//     console.log("you are doing well keep it up the sky is your limit! You got this!")
+//  }
+//     setTimeout(welcome,2000,'Fatima',acollade);
+
+//     //Using setTimeOut and function
+
+//     function height(name){
+//         console.log(name+' '+"below is your height ");
+//     }
+
+//     function theHeight(){
+//         console.log("Tall");
+        
+//     }
+//     setTimeout(theHeight,1000);
+//     height('Yomi');
+
+    
+//     //main function
+//     function studentGrade( name, grade){
+//         console.log('student is'+' '+name)
+//                 grade();
+//     }
+//     // callback function
+//     function grade(exam,test){
+//         //let exam = 50;
+//         //et test= 20;
+//     console.log('grade is'+' '+ 70);
+//     }   
+//       studentGrade('Fatima', grade);
+//       //setTimeout(grade,3000);
+
+//looping
+const scores=[22,54,76,92,43,43]
+ for (let i=0; i <=6; i++){
+    console.log(scores[i]);
+ }
+
+//  function foo(b){
+//     const a = 10;
+//    // const b = 12;
+//     return a + foo(12) + 11
+//  }
+// console. log(a + foo(12) + 11);
